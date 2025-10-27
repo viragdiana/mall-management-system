0c90318699805ec44a60627d0a6d79a0a4fe8c74
viragdiana <virag.da@gmail.com>
2025-10-27 11:35:23 +0200

    initial commit

---
+++ src/main/java/com/example/mallmanagementapplication/MallManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallmanagementapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mallmanagementapplication/MallManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallmanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
