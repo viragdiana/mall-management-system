c1dd6efb35a86833f1d2e66fb32fc0f48ba1def3
viragdiana <virag.da@gmail.com>
2025-12-16 12:39:09 +0200

    la maintenancetask Assignment care afisa numele de worker din UI schimbat in AssignmentID

--- src/main/java/com/example/mallmanagementapplication/controller/MaintenanceTaskController.java
+++ src/main/java/com/example/mallmanagementapplication/controller/MaintenanceTaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/tasks")
-public class MaintenanceTaskController {
+public class  MaintenanceTaskController {
 
     private final MaintenanceTaskService taskService;
     private final StaffAssignmentService assignmentService;
